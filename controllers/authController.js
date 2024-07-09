7f4243a58297ac7f5413a794f43731adb9b53a06
Clifford Musyoka <your_email@example.com>
2024-07-09 08:03:42 +0300

    handle errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,25 @@
-const User = require('../model/user')
+const User = require('../model/user');
+
+//Handle Errors
+const handleErrors = (err) => {
+console.log(err.message, err.code);
+let errors = {email: '', password: ''};
+
+//duplicate error code 
+if(err.code === 11000){
+    errors.email = "That email is already registered";
+    return errors;
+}
+
+//validation errors
+if(err.message.includes('user validation failed')){
+    Object.values(err.errors).forEach(({properties}) => {
+        errors[properties.path] = properties.message;
+    });
+}
+
+return errors;
+}
 
 module.exports.signup_get = (req, res) =>{
     res.render('signup');
@@ -14,9 +35,9 @@ module.exports.signup_post = async (req, res) =>{
     try {
         const user = await User.create({email, password});
         res.status(201).json(user);
-    } catch (error) {
-        console.log(error);
-        res.status(400).send('Error, user not created');
+    } catch (err) {
+    const errors = handleErrors(err)
+        res.status(400).json({ errors });
     }
 }
 
